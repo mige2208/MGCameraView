2157acb165bc61bf88542b0b517889c5e91728ea
mige <mige@migedeiMac.local>
2018-05-16 16:26:22 +0800

    Initial Commit

---
+++ MGCameraViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGCameraViewDemo
+//
+//  Created by mige on 2018/5/16.
+//  Copyright © 2018年 mige.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGCameraViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MGCameraViewDemo
+//
+//  Created by mige on 2018/5/16.
+//  Copyright © 2018年 mige.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
